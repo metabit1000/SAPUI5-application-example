9de1b8b971581fe061bc41727337efa3f611ad85
Alex Aguilera <alex.aguilera.martinez@est.fib.upc.edu>
2020-10-05 13:02:57 +0000

    First push

---
+++ webapp/controller/View.controller.js
@@ -0,0 +1,26 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
+	"use strict";
+
+	return Controller.extend("pruebas.pruebas.controller.View", {
+		onInit: function () {
+
+		},
+		
+		onFilterProducts: function(oEvent) {
+			var prodFilter = [];
+			var sQuery = oEvent.getParameter("query"); //cuando el usuario introduzca un valor a filtrar, genera una query
+			
+			if (sQuery) {
+				prodFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+			
+			var oList = this.byId("productList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(prodFilter);
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"pruebas/pruebas/test/unit/controller/View.controller"
+], function () {
+	"use strict";
+});
