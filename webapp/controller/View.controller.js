e9e739a01a0d371e34e41abbe0af377defd08fe1
Alex Aguilera <alex.aguilera.martinez@est.fib.upc.edu>
2020-10-05 13:28:21 +0000

    Some changes

--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -1,13 +1,24 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (Controller, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/Fragment"
+], function (Controller, Filter, FilterOperator, JSONModel, Fragment ) {
 	"use strict";
 
 	return Controller.extend("pruebas.pruebas.controller.View", {
 		onInit: function () {
-
+			
+			// set data model on view 
+        	var oData = {
+            	recipient : {
+            		name : "prueba"
+            	}
+        	};
+        	
+        	var oModel = new JSONModel(oData);
+        	this.getView().setModel(oModel);
 		},
 		
 		onFilterProducts: function(oEvent) {
@@ -22,6 +33,30 @@ sap.ui.define([
 			var oBinding = oList.getBinding("items");
 			oBinding.filter(prodFilter);
 			//prueba
+		}, 
+		
+		onShow : function() {
+			var oView = this.getView();
+
+			// create dialog lazily
+			if (!this.byId("Dialog")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "pruebas.pruebas.view.Dialog",
+					controller: this
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("Dialog").open();
+			}
+		},
+		
+		onCloseDialog : function () {
+			this.byId("Dialog").close();
 		}
 	});
 });
